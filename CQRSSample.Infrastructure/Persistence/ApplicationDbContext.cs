fbfb67d1a0290c9fd277fcf3c185830e058deabf
Berk Ulusoy <b.ulusoy@baylanwatermeters.com>
2025-03-12 16:48:03 +0300

    Remove commented seed data and improve entity configuration in ApplicationDbContext

--- CQRSSample.Infrastructure/Persistence/ApplicationDbContext.cs
+++ CQRSSample.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -22,31 +22,8 @@ namespace CQRSSample.Infrastructure.Persistence
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Burada entity yapılandırmaları yapılabilir
             modelBuilder.Entity<Product>().Property(p => p.ProductCode).IsRequired().HasMaxLength(100);
             modelBuilder.Entity<Product>().Property(p => p.Consumption).HasColumnType("decimal(18,2)");
-
-            // // Seed data
-            // modelBuilder.Entity<Product>().HasData(
-            //     new Product
-            //     {
-            //         Id = 1,
-            //         Name = "Ürün 1",
-            //         Description = "Ürün 1 açıklaması",
-            //         Price = 100.50m,
-            //         StockQuantity = 10,
-            //         CreatedAt =new DateTime(2000, 2, 1, 0, 0, 0), // SABİT TARİH
-            //     },
-            //     new Product
-            //     {
-            //         Id = 2,
-            //         Name = "Ürün 2",
-            //         Description = "Ürün 2 açıklaması",
-            //         Price = 200.75m,
-            //         StockQuantity = 5,
-            //         CreatedAt = new DateTime(2000, 1, 1, 0, 0, 0), // SABİT TARİH
-            //     }
-            // );
         }
     }
 }
